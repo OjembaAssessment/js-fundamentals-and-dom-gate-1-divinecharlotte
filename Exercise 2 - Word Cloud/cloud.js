4b00b69085b4f56d43ad3c2441398e47483172b6
charlotte divine <divinemaina@gmail.com>
2023-07-24 11:22:19 +0200

    solve word cloud challenge

--- Exercise 2 - Word Cloud/cloud.js
+++ Exercise 2 - Word Cloud/cloud.js
@@ -2,21 +2,32 @@ console.log("Exercise 2 - Word Cloud");
 
 function createWordCloud(str) {
     let wordCounts = {};
-    let words = str.split(/\b/);
+    let words = str.split(/\b(?=\w)/)
 
     for (let i = 0; i < words.length; i++) {
         wordCounts["_" + words[i]] = (wordCounts["_" + words[i]] || 0) + 1;
     }
-
+        delete wordCounts[""];
+    delete wordCounts["_ "];
+    delete wordCounts["_."];
+    delete wordCounts[","];
+    delete wordCounts["."];
+    delete wordCounts["_\n"];
+    var sortedWords = Object.keys(wordCounts).sort(function(a, b) {
+        return wordCounts[b] - wordCounts[a];
+    });
 
    
-    let sortedWords = Object.keys(wordCounts).sort();
+    var first12Words = sortedWords.slice(0, 12);
+
+   
+
 
 
     let wordCloudHTML = '';
-    for (let j = 0; j < sortedWords.length; j++) {
-        let word = sortedWords[j].substring(1);
-        let count = wordCounts[sortedWords[j]];
+    for (let j = 0; j < first12Words.length; j++) {
+        let word = first12Words[j].substring(1);
+        let count = wordCounts[first12Words[j]];
 
 
         let fontSize = 12 + count * 2;
@@ -32,5 +43,5 @@ function createWordCloud(str) {
 
 let paragraph = document.getElementById("myParagraph");
 let textContent = paragraph.textContent || paragraph.innerText;
-// console.log(textContent);
+console.log(textContent);
 createWordCloud(textContent);
